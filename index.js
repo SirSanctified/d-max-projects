99abbe309c0ec133a562147c3b94c0855b8d2dfc
Pritchard Mambambo <mambambopritchard@gmail.com>
2022-12-29 01:50:29 +0200

    Create site's initial design

---
+++ index.js
@@ -0,0 +1,53 @@
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
+})
+
+// animate on scroll
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const entries = document.querySelectorAll('.hidden')
+entries.forEach(entry => observer.observe(entry))
+
+// open and close faqs
+
+const faqs = document.querySelectorAll('.faq')
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open')
+
+        const icon = faq.querySelector('.icon i')
+        if (icon.className === 'uil uil-plus') {
+            icon.className = 'uil uil-minus'
+        } else {
+            icon.className = 'uil uil-plus'
+        }
+    })
+})
+
+// nav bar
+
+const menu = document.querySelector('.nav__menu')
+const openMenu = document.querySelector('#open-menu')
+const closeMenu = document.querySelector('#close-menu')
+
+openMenu.addEventListener('click', () => {
+    menu.style.display = 'block'
+    closeMenu.style.display = 'inline-block'
+    openMenu.style.display = 'none'
+})
+
+closeMenu.addEventListener('click', () => {
+    menu.style.display = 'none'
+    openMenu.style.display = 'inline-block'
+    closeMenu.style.display = 'none'
+})
